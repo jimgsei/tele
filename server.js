319e9749d8b7c84827a4f8cc222696bdb3356b38
jimgsei <59120386+jimgsei@users.noreply.github.com>
2025-01-03 01:05:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,22 @@ app.get('/proxy', (req, res) => {
         return res.status(400).send('URL is required');
     }
 
+    // Verifica si la URL comienza con 'http' y completa la URL si es necesario
+    if (!url.startsWith('http')) {
+        return res.status(400).send('Invalid URL');
+    }
+
+    // Realiza la solicitud HTTP con los headers apropiados
     request(
-        { url, method: 'GET', headers: { 'User-Agent': 'Mozilla/5.0' } },
+        { 
+            url: url, 
+            method: 'GET', 
+            headers: { 
+                'User-Agent': 'Mozilla/5.0', 
+                'Accept': 'application/json', 
+                'Accept-Encoding': 'gzip, deflate, br'
+            }
+        },
         (error, response, body) => {
             if (error) {
                 return res.status(500).send(error.message);
