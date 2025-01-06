fc7338f6fc4e435656b3db367cd6564bd33ce81f
jimgsei <59120386+jimgsei@users.noreply.github.com>
2025-01-06 17:20:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,38 @@
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch'); // Para realizar las solicitudes
+const fetch = require('node-fetch');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Habilitar CORS para todas las rutas
 app.use(cors());
 
 // Ruta proxy para redirigir solicitudes y evitar problemas de CORS
 app.get('/proxy', async (req, res) => {
-  const url = req.query.url; // Obtener la URL de la query string
+  const url = req.query.url;
   if (!url) {
     return res.status(400).send('La URL es requerida');
   }
 
   try {
-    // Realizar la solicitud a la URL de destino
     const response = await fetch(url, {
       method: 'GET',
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         'Accept': '*/*',
         'Connection': 'keep-alive',
-        'Origin': req.headers.origin || 'null', // Pasar la cabecera Origin si está presente
       },
-      redirect: 'follow', // Configurar para seguir automáticamente las redirecciones
+      redirect: 'follow',
     });
 
-    // Enviar la respuesta final al cliente
-    const data = await response.text(); // Puedes usar .json() si esperas una respuesta JSON
+    const data = await response.text();
     res.send(data);
-
   } catch (error) {
     console.error(`Error al realizar la solicitud proxy: ${url}`, error);
     res.status(500).send('Error al realizar la solicitud proxy');
   }
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor proxy corriendo en http://localhost:${port}`);
 });
